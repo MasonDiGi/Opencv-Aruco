92e5c8c7433eaf3b3ddd040b61a9baa581d21212
Sunwoo Park <sunwooid@gmail.com>
2021-02-19 16:08:57 -0500

    Moved Matrix Functions to Separate Python File
    It looks cleaner

--- aruco_pose.py
+++ aruco_pose.py
@@ -9,71 +9,9 @@ from consts import MARKER_LOCATIONS
 import configparser
 import numpy as np
 import cameracal
+import matrixfunctions as mf
 import threading
-import math
 from networktables import NetworkTables
-
-def poseToMatrix(x, y, theta):
-    # If this function doesn't work nothing else does
-    cosTheta = np.cos(theta)
-    sinTheta = np.sin(theta)
-    arr = np.matrix([[cosFieldStart, -sinFieldStart, xFieldStart],
-        [sinFieldStart, cosFieldStart, yFieldStart],
-        [0, 0, 1]])
-    return arr
-
-def matrixToPose(arr):
-    # I think I have to do some sign flipping with this arccos to prevent wrap around error
-    # I think it was something like
-    # if sin(theta) < 0 then -arccos(x)
-    # else arccos(x)
-    # Don't remember tbh need testing
-    return [arr[0][2], arr[1][2], np.arccos(arr[0][0])]
-
-def matrixTransform(arr1, arr2):
-    # might be more useful to not make this a function and just do the matmul inline
-    return np.matmul(arr1,np.linalg.inv(arr2))
-    # matrixMarker * matrixCamera^-1
-
-
-def getMarkerMatrix(id):
-    # get marker pose from dictionary
-    x = MARKER_LOCATIONS[id][0]
-    y = MARKER_LOCATIONS[id][1]
-    theta = MARKER_LOCATIONS[id][2]
-    # convert that pose to matrix and return
-    return poseToMatrix(x,y,theta)
-
-# Checks if a matrix is a valid rotation matrix.
-def isRotationMatrix(R) :
-    Rt = np.transpose(R)
-    shouldBeIdentity = np.dot(Rt, R)
-    I = np.identity(3, dtype = R.dtype)
-    n = np.linalg.norm(I - shouldBeIdentity)
-    return n < 1e-6
-
-
-# Calculates rotation matrix to euler angles
-# The result is the same as MATLAB except the order
-# of the euler angles ( x and z are swapped ).
-def rotationMatrixToEulerAngles(R) :
-
-    assert(isRotationMatrix(R))
-    
-    sy = math.sqrt(R[0,0] * R[0,0] +  R[1,0] * R[1,0])
-    
-    singular = sy < 1e-6
-
-    if  not singular :
-        x = math.atan2(R[2,1] , R[2,2])
-        y = math.atan2(-R[2,0], sy)
-        z = math.atan2(R[1,0], R[0,0])
-    else :
-        x = math.atan2(-R[1,2], R[1,1])
-        y = math.atan2(-R[2,0], sy)
-        z = 0
-
-    return np.array([x, y, z])
 
 # Initialize NetworkTables Connection
 # Wait for server to connect
@@ -143,7 +81,7 @@ while True:
             cv2.aruco.drawAxis(frame, mtx,
                                dist, rvecs[i], tvecs[i], 0.035)
 
-            # robotPose = matrixToPose(matrixTransform(getMarkerMatrix(i),poseToMatrix(tvecs[i][0],tvecs[i][2],rvecs[i][0])))\
+            # robotPose = mf.matrixToPose(mf.matrixTransform(mf.getMarkerMatrix(i),mf.poseToMatrix(tvecs[i][0],tvecs[i][2],rvecs[i][0])))
             # rX = robotPose[0]
             # rY = robotPose[1]
             # rTheta = robotPose[2]
@@ -152,7 +90,7 @@ while True:
 		    # table.putNumber('robotTheta:', rTheta)
 
             rot, _ = cv2.Rodrigues(rvecs[0])
-        print(str(rotationMatrixToEulerAngles(rot)))
+        print(str(mf.rotationMatrixToEulerAngles(rot)))
         #print(str(rot[2][1]) + " " + str(rot[0][2]) + " " + str(rot[1][0]))
 
 
